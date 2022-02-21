0214876dd09198653f181d674758417d3823975e
Ruslan-Sh-sudo <ruslan2411sh@gmail.com>
2022-02-21 10:24:37 +0200

    finished joins lab

--- joins.sql
+++ joins.sql
@@ -1,6 +1,30 @@
 SELECT p.firstname, p.middlename, p.lastname, e.jobtitle, e.birthdate
-    FROM person.person p 
-    JOIN humanresources.employee e 
+    FROM person.person p
+    JOIN humanresources.employee e
       ON (p.businessentityid = e.businessentityid)
 ORDER BY p.firstname ASC;
 
+SELECT p.firstname, p.lastname,
+(SELECT e.jobtitle
+   FROM humanresources.employee e
+  WHERE p.businessentityid = e.businessentityid) AS jobtitle
+   FROM person.person p;
+
+
+SELECT * FROM(
+SELECT p.firstname, p.lastname,
+(SELECT e.jobtitle
+   FROM humanresources.employee e
+  WHERE p.businessentityid = e.businessentityid) AS jobtitle
+   FROM person.person p) AS table1
+   WHERE jobtitle IS NOT null;
+   
+    SELECT p.firstname, p.lastname, e.jobtitle
+      FROM person.person p
+CROSS JOIN humanresources.employee e;
+
+SELECT COUNT(*) FROM
+(SELECT p.firstname, p.lastname, e.jobtitle
+      FROM person.person p
+CROSS JOIN humanresources.employee e) AS counting;
+   
